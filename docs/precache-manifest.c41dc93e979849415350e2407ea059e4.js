c77ff761a7d0e75257a5035b1702d7c45d6c404c
babak <babak.zarrinbal@gmail.com>
2019-12-21 14:07:24 +0330

    init commit

---
+++ docs/precache-manifest.c41dc93e979849415350e2407ea059e4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47ea0e8c5b4d13df138",
+    "url": "/aknoon/css/app.e2713bb0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/aknoon/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/aknoon/img/pwa-icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/aknoon/img/pwa-icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/aknoon/img/pwa-icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/aknoon/img/pwa-icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/aknoon/img/pwa-icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/aknoon/img/pwa-icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/aknoon/img/pwa-icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/aknoon/img/pwa-icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/aknoon/img/pwa-icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/aknoon/img/pwa-icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/aknoon/img/pwa-icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/aknoon/img/pwa-icons/mstile-150x150.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/aknoon/img/pwa-icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "6349fdea5a22f6a5b1ea91525876f17a",
+    "url": "/aknoon/index.html"
+  },
+  {
+    "revision": "e47ea0e8c5b4d13df138",
+    "url": "/aknoon/js/app.195f1b05.js"
+  },
+  {
+    "revision": "56485ba7ca7331d7f361",
+    "url": "/aknoon/js/chunk-vendors.beea1eda.js"
+  },
+  {
+    "revision": "423edbbf9ccb0e1b440ee275a4aae16f",
+    "url": "/aknoon/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/aknoon/robots.txt"
+  }
+]);
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
-
+require("./serviceworker/registerServiceWorker");
 new Vue({
   render: h => h(App),
 }).$mount('#app')
