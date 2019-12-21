0554dcce2512517110d1c385f3883048319f1771
babak <babak.zarrinbal@gmail.com>
2019-12-21 17:00:22 +0330

    fixing service worker buggs

--- src/serviceworker/service-worker.js
+++ src/serviceworker/service-worker.js
@@ -1,62 +1,73 @@
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open('offlinecaches').then(function(cache) {
-      return cache.addAll(
-        [
-          '/aknoon/index.html'
-        ]
-      );
-    })
-  );
-});
 
-self.addEventListener("activate", function(event) {
-  // console.log("Service Worker activating.");
-});
-self.addEventListener("fetch", function(event) {
-  event.respondWith(function(){
-    return caches.match(event.request).then(function(response) {
-      // console.log('response',response.text());
-      // response.then('cacheresult',console.log);
-        if (response) {
-            // retrieve from cache
-            return response;
-        }
-        // if not found in cache, return default offline content (only if this is a navigation request)
-        if (event.request.mode === 'navigate') {
-            return caches.match('/aknoon/index.html');
-        }
 
-        // fetch as normal
-        return fetch(event.request);
+workbox.setConfig({ modulePathPrefix: "/aknoon/workbox-v4.3.1", debug:false });
 
-      });
-    });
-});
+workbox.precaching.precacheAndRoute(
+  self.__precacheManifest
+);
+workbox.routing.registerRoute(
+  new RegExp('\\.[png|jpg|svg|html|js|css]$'),
+  new workbox.strategies.CacheFirst()
+);
+// self.addEventListener("install", function(event) {
+//   event.waitUntil(
+//     caches.open('offlinecaches').then(function(cache) {
+//       return cache.addAll(
+//         [
+//           '/aknoon/index.html'
+//         ]
+//       );
+//     })
+//   );
+// });
 
-self.addEventListener("push", function(event) {
-  // var data = event.data.json();
-  // const title = data.title || "Driver @ Dilivir";
-  // const options = {
-  //   body: data.body || "New orders",
-  //   icon: data.icon || "img/icons/logo.png",
-  //   badge: data.badge || "img/icons/logo.png",
-  //   data,
-  // };
+// self.addEventListener("activate", function(event) {
+//   // console.log("Service Worker activating.");
+// });
+// self.addEventListener("fetch", function(event) {
+//   event.respondWith(function(){
+//     return caches.match(event.request).then(function(response) {
+//       // console.log('response',response.text());
+//       // response.then('cacheresult',console.log);
+//         if (response) {
+//             // retrieve from cache
+//             return response;
+//         }
+//         // if not found in cache, return default offline content (only if this is a navigation request)
+//         if (event.request.mode === 'navigate') {
+//             return caches.match('/zmovies/index.html');
+//         }
 
-  // if (data.tag) options.tag = data.tag;
-  // const notificationPromise = self.registration.showNotification(
-  //   title,
-  //   options
-  // );
-  // event.waitUntil(notificationPromise);
-});
+//         // fetch as normal
+//         return fetch(event.request);
 
-self.addEventListener("notificationclick", function(event) {
-  // event.notification.close();
-  // event.waitUntil(
-  //   clients.openWindow(
-  //     "http://localhost:5000/#/" + (event.notification.data || {}).gotourl || ""
-  //   )
-  // );
-});
+//       });
+//     });
+// });
+
+// self.addEventListener("push", function(event) {
+//   // var data = event.data.json();
+//   // const title = data.title || "Driver @ Dilivir";
+//   // const options = {
+//   //   body: data.body || "New orders",
+//   //   icon: data.icon || "img/icons/logo.png",
+//   //   badge: data.badge || "img/icons/logo.png",
+//   //   data,
+//   // };
+
+//   // if (data.tag) options.tag = data.tag;
+//   // const notificationPromise = self.registration.showNotification(
+//   //   title,
+//   //   options
+//   // );
+//   // event.waitUntil(notificationPromise);
+// });
+
+// self.addEventListener("notificationclick", function(event) {
+//   // event.notification.close();
+//   // event.waitUntil(
+//   //   clients.openWindow(
+//   //     "http://localhost:5000/#/" + (event.notification.data || {}).gotourl || ""
+//   //   )
+//   // );
+// });
